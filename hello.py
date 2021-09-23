af148fd6fd15400453fc3851ac7203a774ff61a6
Gabrielle Madden <49839049+gabriellemadden@users.noreply.github.com>
2021-09-22 22:43:22 -0400

    4.6 to 4.7 - Add flash messages for name changes

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import Form
@@ -18,12 +18,17 @@ app.config['SECRET_KEY'] = 'key'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
 
     if form.validate_on_submit():
+        old_name = session.get('name')
+        print(old_name)
+        print(form.name.data)
+
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+
         session['name'] = form.name.data
-        form.name.data = ''
         return redirect(url_for('index'))
 
     return render_template(
