90c421d6ec432ce3e77c9c52cc2496554e208500
Gabrielle Madden <49839049+gabriellemadden@users.noreply.github.com>
2021-09-22 22:26:44 -0400

    4.2 to 4.4 - Add name form to home page

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 from datetime import datetime
 
@@ -13,10 +16,21 @@ moment = Moment(app)
 # exposed in this github repo anyways
 app.config['SECRET_KEY'] = 'key'
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html',
-    current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+
+    return render_template(
+        'index.html',
+        current_time=datetime.utcnow(),
+        form=form,
+        name=name
+    )
 
 @app.route('/user/<name>')
 def user(name):
@@ -29,6 +43,10 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
 
 
 if __name__ == '__main__':
